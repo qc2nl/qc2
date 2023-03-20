bf4c910ac90bed9fea76572944cd6deb87ccf97a
Cmurilochem <carlosmurilorocha@gmail.com>
2023-03-20 16:50:55 +0100

    updated tests with sto-3g and comments

--- tests/test_pyscf_ase.py
+++ tests/test_pyscf_ase.py
@@ -12,12 +12,15 @@ from qc2.ase.pyscf import PySCF
 def test_PySCF_energy_rks():
     """Test case # 1 - RKS H2 molecule"""
 
+    # define molecule geometry
     h2_molecule = Atoms('H2', positions=[[0, 0, 0], [0, 0, 0.7]])
 
-    h2_molecule.calc = PySCF(method='dft.RKS', xc='b3lyp', basis='6-31g*', charge=0, multiplicity=1, verbose=0)
+    # run calc and convert electronic energy into atomic units 
+    h2_molecule.calc = PySCF(method='dft.RKS', xc='pbe', basis='sto-3g', charge=0, multiplicity=1, verbose=0)
     energy_Eh = h2_molecule.get_potential_energy() / Ha
 
-    assert energy_Eh == pytest.approx(-1.1673385906283245, rel=1e-5)
+    # compare with the energy obtained using pyscf alone - assuming convergence up to ~1e-6
+    assert energy_Eh == pytest.approx(-1.15101082903195, 1e-6)
 
 
 def test_PySCF_energy_uhf():
@@ -25,34 +28,44 @@ def test_PySCF_energy_uhf():
     
     h_atom = Atoms('H')
 
-    h_atom.calc = PySCF(method='scf.UHF', basis='6-31g*', charge=0, multiplicity=2, verbose=0)
+    h_atom.calc = PySCF(method='scf.UHF', basis='sto-3g', charge=0, multiplicity=2, verbose=0)
     energy_Eh = h_atom.get_potential_energy() / Ha
 
-    assert energy_Eh == pytest.approx(-0.5, rel=0.01)
+    assert energy_Eh == pytest.approx(-0.466581849557275, 1e-6)
 
 def test_PySCF_energy_rohf():
     """Test case # 3 - ROHF O2 molecule"""
 
+    # now using the built-in molecule dataset to define the geometry 
     o2_molecule = molecule('O2')
     
+    # define the calculator
     o2_molecule.calc = PySCF()
+
+    # as an alternative to #1, define PySCF wave function using its specific attributes  
     o2_molecule.calc.method = 'scf.ROHF'
-    o2_molecule.calc.basis = '6-31g*'
+    o2_molecule.calc.basis = 'sto-3g'
     o2_molecule.calc.charge = 0 
     o2_molecule.calc.multiplicity = 3
     o2_molecule.calc.verbose = 0
     energy_Eh = o2_molecule.get_potential_energy() / Ha
 
-    assert energy_Eh == pytest.approx(-149.58311940650503, rel=1e-5)
+    assert energy_Eh == pytest.approx(-147.630640704849, 1e-6)
 
-def test_PySCF_forces():
-    """Test case # 4 - RKS forces H2 molecule"""
+def test_PySCF_energy_opt():
+    """Test case # 4 - RKS geometry optimization H2 molecule"""
 
+    # define geometry and wave function
     h2_molecule = molecule('H2')
-    h2_molecule.calc = PySCF(method='dft.RKS', xc='b3lyp', basis='6-31g*', charge=0, multiplicity=1, verbose=0)
+    h2_molecule.calc = PySCF(method='dft.RKS', xc='pbe', basis='sto-3g', charge=0, multiplicity=1, verbose=0)
+
+    # run optimization and get the energy at minium
     opt = BFGS(h2_molecule)
     opt.run(fmax=1e-4)
-    energy_opt = h2_molecule.get_potential_energy()
+    energy_opt = h2_molecule.get_potential_energy() / Ha
+
+    # Just an example, you can also calculate the gradient
     gradient_opt = h2_molecule.get_forces()
 
-    assert gradient_opt == pytest.approx(np.zeros((2, 3)), abs=1e-6) 
+    # now compare with the energy obtained using pyscf pyberny optimization alone
+    assert energy_opt == pytest.approx(-1.15209884495, 1e-6)
