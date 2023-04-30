e2187c63a505a51b7294b1eefcedb13a8bccf805
Cmurilochem <carlosmurilorocha@gmail.com>
2023-04-30 11:10:10 +0200

    Updated directories

--- qc2/ase/pyscf.py
+++ qc2/ase/pyscf.py
@@ -1,10 +1,12 @@
-"""This module defines an ASE interface to PySCF
+"""This module defines an ASE interface to PySCF.
 
-https://pyscf.org/ => Official documentation             
-https://github.com/pyscf/pyscf => GitHub page 
+https://pyscf.org/ => Official documentation
+https://github.com/pyscf/pyscf => GitHub page
 
-Note: Adapted from https://github.com/pyscf/pyscf/blob/master/pyscf/pbc/tools/pyscf_ase.py
-               and https://github.com/pyscf/pyscf/issues/624
+Note: Adapted from
+https://github.com/pyscf/pyscf/blob/master/pyscf/pbc/tools/pyscf_ase.py
+&
+https://github.com/pyscf/pyscf/issues/624
 """
 from typing import Union, Optional, List, Dict, Any
 import warnings
@@ -15,6 +17,7 @@ from ase.units import Ha, Bohr
 from ase.calculators.calculator import InputError
 from ase.calculators.calculator import CalculatorSetupError
 
+
 def ase_atoms_to_pyscf(ase_atoms: Atoms) -> List[List[Union[str, np.ndarray]]]:
     """Converts ASE atoms to PySCF atom.
 
@@ -24,21 +27,23 @@ def ase_atoms_to_pyscf(ase_atoms: Atoms) -> List[List[Union[str, np.ndarray]]]:
     Returns:
         List[List[Union[str, np.ndarray]]]: PySCF atom.
     """
-    return [ [ase_atoms.get_chemical_symbols()[i], ase_atoms.get_positions()[i]] for i in range(len(ase_atoms.get_positions()))]
+    return [[ase_atoms.get_chemical_symbols()[i], ase_atoms.get_positions()[i]]
+            for i in range(len(ase_atoms.get_positions()))]
 
 
 class PySCF(Calculator):
-    """An ASE calculator for the open source (python-based) PySCF quantum chemistry package.
+    """An ASE calculator for the PySCF quantum chemistry package.
 
     Args:
-        Calculator (Calculator): Base-class for all ASE calculators. 
+        Calculator (Calculator): Base-class for all ASE calculators.
 
     Raises:
-        InputError: If attributes other than 
-            'method', 'xc', 'basis', 'multiplicity', 
-            'charge', and 'verbose' are input as Calculator. 
-        CalculatorSetupError: If abinitio methods other than 
-            'scf.RHF', 'scf.UHF', 'scf.ROHF', 'dft.RKS', 'dft.UKS', and 'dft.ROKS' are selected.
+        InputError: If attributes other than
+            'method', 'xc', 'basis', 'multiplicity',
+            'charge', and 'verbose' are input as Calculator.
+        CalculatorSetupError: If abinitio methods other than
+            'scf.RHF', 'scf.UHF', 'scf.ROHF',
+            'dft.RKS', 'dft.UKS', and 'dft.ROKS' are selected.
 
     Example of a typical ASE-PySCF input:
 
@@ -47,11 +52,16 @@ class PySCF(Calculator):
     >>> from qc2.ase.pyscf import PySCF
     >>>
     >>> molecule = Atoms(...) or molecule = molecule('...')
-    >>> molecule.calc = PySCF(method='dft.RKS', xc='b3lyp', basis='6-31g*', charge=0, multiplicity=1, verbose=0)
+    >>> molecule.calc = PySCF(method='dft.RKS',
+                            xc='b3lyp',
+                            basis='6-31g*',
+                            charge=0,
+                            multiplicity=1,
+                            verbose=0)
     >>> energy = molecule.get_potential_energy()
     >>> gradient = molecule.get_forces()
 
-    where (currently) 
+    where (currently)
 
     method = 'scf.RHF'
            = 'scf.UHF'
@@ -64,116 +74,138 @@ class PySCF(Calculator):
 
     default_parameters: Dict[str, Any] = {'method': 'dft.RKS',
                                           'basis': '6-31g*',
-                                          'xc': 'b3lyp', 
+                                          'xc': 'b3lyp',
                                           'multiplicity': 1,
                                           'charge': 0,
                                           'verbose': 0}
 
     def __init__(self,
-                 restart : Optional[bool] = None,
+                 restart: Optional[bool] = None,
                  ignore_bad_restart: Optional[bool] = False,
-                 label : Optional[str] = 'PySCF',
-                 atoms : Optional[Atoms] = None,
-                 command : Optional[str] = None,
-                 directory : str = '.',
+                 label: Optional[str] = 'PySCF',
+                 atoms: Optional[Atoms] = None,
+                 command: Optional[str] = None,
+                 directory: str = '.',
                  **kwargs) -> None:
-        """ASE-PySCF Class Constructor to initialize the object. 
-        
-        Note: Basic implementation based on the class Psi4(Calculator); see, e.g., ase/ase/calculators/psi4.py.
+        """ASE-PySCF Class Constructor to initialize the object.
+
+        Note: Basic implementation based on the
+            class Psi4(Calculator); see, e.g., ase/ase/calculators/psi4.py.
 
         Args:
-            restart (bool, optional): Prefix for restart file. 
+            restart (bool, optional): Prefix for restart file.
                 May contain a directory. Defaults to None: don't restart.
-            ignore_bad_restart (bool, optional): Deprecated and will stop working in the future. Defaults to False.
+            ignore_bad_restart (bool, optional): Deprecated and will
+                stop working in the future. Defaults to False.
             label (str, optional): Calculator name. Defaults to 'PySCF'.
-            atoms (Atoms, optional): Atoms object to which the calculator will be attached. 
-                When restarting, atoms will get its positions and unit-cell updated from file. Defaults to None.
-            command (str, optional): Command used to start calculation. Defaults to None.
-            directory (str, optional): Working directory in which perform calculations. Defaults to '.'.
+            atoms (Atoms, optional): Atoms object to which the calculator
+                will be attached. When restarting, atoms will get its
+                positions and unit-cell updated from file. Defaults to None.
+            command (str, optional): Command used to start calculation.
+                Defaults to None.
+            directory (str, optional): Working directory in which
+                to perform calculations. Defaults to '.'.
         """
-
-        # initializing base class Calculator; see ase/ase/calculators/calculator.py.
+        # initializing base class Calculator.
+        # see ase/ase/calculators/calculator.py.
         super().__init__(restart=restart,
                          ignore_bad_restart=ignore_bad_restart, label=label,
                          atoms=atoms, command=command, directory=directory,
                          **kwargs)
         """Transforms **kwargs into a dictionary with calculation parameters.
-        
-        Starting with (attr1=value1, attr2=value2, ...) it creates self.parameters['attr1']=value1, and so on.
+
+        Starting with (attr1=value1, attr2=value2, ...)
+            it creates self.parameters['attr1']=value1, and so on.
         """
 
         # Check self.parameters input keys and values
         self.check_pyscf_attributes()
 
     def check_pyscf_attributes(self) -> None:
-        """This method checks for any missing and/or mispelling PySCF input attribute.
+        """Checks for any missing and/or mispelling PySCF input attribute.
 
-        Note: it can also be used to eventually set specific environment variables, ios, etc. 
+        Note: it can also be used to eventually set specific
+        environment variables, ios, etc.
         """
-
-        recognized_attributes: List[str] = ['ignore_bad_restart', 'command', 'method',
-                                            'xc', 'basis', 'multiplicity', 'charge',
-                                            'verbose', 'kpts', 'nbands', 'smearing']
+        recognized_attributes: List[str] = [
+            'ignore_bad_restart', 'command', 'method',
+            'xc', 'basis', 'multiplicity', 'charge',
+            'verbose', 'kpts', 'nbands', 'smearing'
+            ]
 
         # self.parameters gathers all PYSCF input options in a dictionary.
-        # it is defined by class Calculator(BaseCalculator); see ase/ase/calculators/calculator.py
+        # it is defined by class Calculator(BaseCalculator)
+        # see ase/ase/calculators/calculator.py
         for key, value in self.parameters.items():
             # check attribute name
             if key not in recognized_attributes:
-                raise InputError('Attribute', key, 'not recognized. Please check input.')
+                raise InputError('Attribute', key,
+                                 ' not recognized. Please check input.')
 
         # dealing with lack of multiplicity and charge info
         if 'multiplicity' not in self.parameters.keys():
             self.parameters['multiplicity'] = 1
-            warnings.warn('Multiplicity not provided. Assuming default singlet.')
+            warnings.warn('Multiplicity not provided.'
+                          'Assuming default singlet.')
 
         if 'charge' not in self.parameters.keys():
             self.parameters['charge'] = 0
             warnings.warn('Charge not provided. Assuming default zero.')
 
-        # verbose sets the amount of pyscf printing => verbose = 0 prints no info
+        # verbose sets the amount of pyscf printing
+        # verbose = 0 prints no info
         if 'verbose' not in self.parameters.keys():
             self.parameters['verbose'] = 0
 
         # dealing with some ASE specific inputs
         if 'kpts' in self.parameters.keys():
-            raise InputError('This ASE-PySCF interface does not yet implement periodic calculations, and thus does not'
-                             ' accept k-points as parameters. Please remove this '
-                             'argument.')
+            raise InputError('This ASE-PySCF interface does not yet implement'
+                             ' periodic calculations, and thus does not'
+                             ' accept k-points as parameters. '
+                             'Please remove this argument.')
 
         if 'nbands' in self.parameters.keys():
-            raise InputError('This ASE-PySCF interface does not support the keyword "nbands".')
+            raise InputError('This ASE-PySCF interface '
+                             'does not support the keyword "nbands".')
 
         if 'smearing' in self.parameters.keys():
-            raise InputError('Finite temperature DFT is not currently implemented in'
-                             ' this PySCF-ASE interface, thus a smearing argument '
-                             'cannot be utilized. Please remove this '
-                             'argument.')    
+            raise InputError('Finite temperature DFT is not currently'
+                             ' implemented in this PySCF-ASE interface,'
+                             ' thus a smearing argument cannot be utilized.'
+                             ' Please remove this argument.')
 
-    def calculate(self, atoms: Optional[Atoms]=None, properties: List[str]=['energy'], system_changes: List[str]=all_changes) -> None:
+    def calculate(self, atoms: Optional[Atoms] = None,
+                  properties: List[str] = ['energy'],
+                  system_changes: List[str] = all_changes) -> None:
         """This method is the core responsible for the actual calculation.
 
-        Note: Implementation based on Calculator.calculate() method; see also ase/ase/calculators/calculator.py. 
+        Note: Implementation based on Calculator.calculate() method.
+            see also ase/ase/calculators/calculator.py.
 
         Args:
-            atoms (Atoms, optional): Atoms object to which the calculator is attached. Defaults to None.
-            properties (list[str], optional): List of what properties need to be calculated. Defaults to ['energy'].
-            system_changes (list[str], optional): List of what has changed since last calculation.
-                Can be any combination of these six: 'positions', 'numbers', 'cell', 'pbc', 
-                'initial_charges' and 'initial_magmoms'. Defaults to all_changes.
+            atoms (Atoms, optional): Atoms object to which
+                the calculator is attached. Defaults to None.
+            properties (list[str], optional): List of what properties
+                need to be calculated. Defaults to ['energy'].
+            system_changes (list[str], optional): List of what has changed
+                since last calculation. Can be any combination
+                of these six: 'positions', 'numbers', 'cell', 'pbc',
+                'initial_charges' and 'initial_magmoms'.
+                Defaults to all_changes.
 
         Raises:
             CalculatorSetupError: If a proper geometry is not provided.
-            CalculatorSetupError: If abinitio methods other than 
-            'scf.RHF', 'scf.UHF', 'scf.ROHF', 'dft.RKS', 'dft.UKS', and 'dft.ROKS' are selected.    
+            CalculatorSetupError: If abinitio methods other than
+            'scf.RHF', 'scf.UHF', 'scf.ROHF', 'dft.RKS', 'dft.UKS',
+                and 'dft.ROKS' are selected.
         """
-
         implemented_methods: List[str] = ['scf.RHF', 'scf.UHF', 'scf.ROHF',
                                           'dft.RKS', 'dft.UKS', 'dft.ROKS']
 
         from pyscf import gto, scf, dft
 
-        # setting up self.atoms attribute from base class Calculator.calculate. This is extracted from the atoms Atoms object.
+        # setting up self.atoms attribute from base class Calculator.calculate.
+        # this is extracted from the atoms Atoms object.
         super().calculate(atoms=atoms)
 
         # checking that self.atoms has been properly initiated/updated
@@ -192,25 +224,27 @@ class PySCF(Calculator):
 
         # checking wf input name => this is case sensitive
         if self.parameters['method'] in implemented_methods:
-            wf = eval(self.parameters['method'])(molecule)
+            self.wf = eval(self.parameters['method'])(molecule)
         else:
             raise CalculatorSetupError('Method not yet implemented. '
-                                       'Current PySCF-ASE calculator only allows for', 
+                                       'Current PySCF-ASE calculator '
+                                       'only allows for',
                                        implemented_methods,
-                                       'wave functions. Please check input method.')
+                                       'wave functions.'
+                                       ' Please check input method.')
 
-        wf.verbose = self.parameters['verbose']
+        self.wf.verbose = self.parameters['verbose']
 
         if 'dft' in self.parameters['method']:
-            wf.xc = self.parameters['xc']
+            self.wf.xc = self.parameters['xc']
 
         # calculating energy in eV
-        energy = wf.kernel() * Ha
+        energy = self.wf.kernel() * Ha
         self.results['energy'] = energy
 
         # calculating forces
         if 'forces' in properties:
-            gf = wf.nuc_grad_method()
+            gf = self.wf.nuc_grad_method()
             gf.verbose = self.parameters['verbose']
             if 'dft' in self.parameters['method']:
                 gf.grid_response = True
--- qc2/ase/rose.py
+++ qc2/ase/rose.py
@@ -1,5 +1,235 @@
-# from ase.calculators import GenericFileIOCalculator
+"""This module defines an ASE interface to Rose.
 
-# class ROSE():
+Original paper & official release:
+https://pubs.acs.org/doi/10.1021/acs.jctc.0c00964
 
-#     def __init__(self):
+GitLab page:
+https://gitlab.com/quantum_rose/rose
+
+Note: see also https://pubs.acs.org/doi/10.1021/ct400687b.
+"""
+from dataclasses import dataclass, field
+from enum import Enum
+from ase import Atoms
+from ase.calculators.calculator import FileIOCalculator
+from typing import Optional, List, Union, Sequence
+
+
+class RoseCalcType(Enum):
+    """Enumerator class defining the type of calculation done by Rose."""
+    ATOM_FRAG = 'atom_frag'  # calc IAOs from atom-free AOs.
+    MOL_FRAG = 'mol_frag'    # calc IFOs from free molecular fragment MOs.
+
+
+class RoseInterfaceMOCalculators(Enum):
+    """Enumerator class defining the program to which Rose is interfaced."""
+    PYSCF = 'pyscf'
+    PSI4 = 'psi4'
+    DIRAC = 'dirac'
+    ADF = 'adf'
+    GAUSSIAN = 'gaussian'
+
+
+class RoseIFOVersion(Enum):
+    """Enumerator class defining the 'recipe' to obtain IFOs."""
+    STNDRD_2013 = "Stndrd_2013"
+    SIMPLE_2013 = "Simple_2013"
+    SIMPLE_2014 = "Simple_2014"
+
+
+class RoseILMOExponent(Enum):
+    """Enumerator class defining the exponent used to obtain ILMOs."""
+    TWO = 2
+    THREE = 3
+    FOUR = 4
+
+
+@dataclass
+class RoseInputDataClass:
+    """A dataclass representing input options for Rose."""
+    rose_target: Atoms
+    rose_frags: Union[Atoms, Sequence[Atoms]]
+
+    calculate_mo: bool = True
+    rose_calc_type: RoseCalcType = RoseCalcType.ATOM_FRAG.value
+
+    run_postscf: bool = False
+    restricted: bool = True
+    openshell: bool = False
+    relativistic: bool = False
+
+    # Rose intrinsic options => expected to be fixed at the default values?
+    version: RoseIFOVersion = RoseIFOVersion.STNDRD_2013.value
+    exponent: RoseILMOExponent = RoseILMOExponent.FOUR.value
+    spherical: bool = False
+    uncontract: bool = True
+    test: bool = True
+    wf_restart: bool = True
+    get_oeint: bool = True
+    save: bool = True
+    avas_frag: Optional[List[int]] = field(default_factory=list)
+    nmo_avas: Optional[List[int]] = field(default_factory=list)
+
+    def __post_init__(self):
+        """This method is called after the instance is initialized."""
+        if self.test:
+            self.get_oeint = True
+
+    """Description of the attributes to be added below."""
+
+
+class Rose(RoseInputDataClass, FileIOCalculator):
+    """A general ASE calculator for ROSE (Reduction of Orbital Space Extent).
+
+    Args:
+        RoseInputDataClass (RoseInputDataClass): Tailor-made base dataclass
+            for Rose; see above.
+        FileIOCalculator (FileIOCalculator): Base class for ASE calculators
+            that write/read input/output files.
+
+    """
+    implemented_properties: List[str] = []
+    command: str = 'echo "Executing Rose...done"'
+    restart: bool = None
+    ignore_bad_restart_file: bool = FileIOCalculator._deprecated
+    label: str = 'rose'
+    atoms: Atoms = None
+    _directory = '.'
+
+    def __init__(self, *args, **kwargs) -> None:
+        """ASE-Rose Class Constructor to initialize the object.
+
+        Give an example here on how to use....
+        """
+        # initializing base classes
+        # RoseInputDataClass.__init__(self, *args, **kwargs)
+        # FileIOCalculator.__init__(self, *args, **kwargs)
+        super().__init__(*args, **kwargs)
+
+        # print()
+
+    def calculate(self, *args, **kwargs) -> None:
+        """Executes Rose workflow."""
+        super().calculate(*args, **kwargs)
+        self.generate_input_genibo_avas()
+        self.generate_input_xyz()
+
+        if self.calculate_mo:
+            self.generate_mo_files()
+
+        self.run_rose()
+
+        if self.save:
+            self.save_ibos()
+
+        if self.avas_frag:
+            self.run_avas()
+
+        if self.run_postscf:
+            self.run_post_hf()
+
+    def generate_input_genibo_avas(self) -> None:
+        """Generates INPUT_GENIBO & INPUT_AVAS fortran files for Rose."""
+        genibo_filename = 'INPUT_GENIBO'
+        avas_filename = 'INPUT_AVAS'
+
+        write_fortran_genibo_avas_input(self, genibo_filename, avas_filename)
+
+    def generate_input_xyz(self) -> None:
+        """Generates Molecule and Fragment xyz files for Rose."""
+        print("Creating Molecule and Frags inputs....done")
+
+    def generate_mo_files(self) -> None:
+        """Generates atomic and molecular orbitals files for Rose."""
+        print('Calculating MO files....done')
+
+    def run_rose(self) -> None:
+        """Runs Rose executable 'genibo.x'."""
+        print('Executing Rose....done')
+
+    def save_ibos(self) -> None:
+        """Generates a checkpoint file ('ibo.chk') with the final IBOs."""
+        print('Saving ibos....done')
+
+    def run_avas(self) -> None:
+        """Runs 'avas.x' executable."""
+        print('Executing avas....done')
+
+    def run_post_hf(self) -> None:
+        """Performs CASCI and/or CASSCF calculations."""
+        print('CASSCF?....done')
+
+
+def write_fortran_genibo_avas_input(
+        input_data: RoseInputDataClass,
+        genibo_filename: str = 'INPUT_GENIBO',
+        avas_filename: str = 'INPUT_AVAS') -> None:
+    """Writes Rose fortran inputs.
+
+    Args:
+        input_data (RoseInputDataClass): dataclass defining input
+            options for Rose.
+        genibo_filename (str): file to be generated containing
+            Rose input options.
+        avas_filename (str): name of the file to be generated with
+            AVAS input options.
+    """
+    print("Creating genibo and avas input....done")
+
+    molecule_charge = input_data.rose_target.calc.parameters.charge
+    molecule_mo_calculator = input_data.rose_target.calc.name.lower()
+    molecule_natom = len(input_data.rose_target.symbols)
+
+    fragments_mo_calculator = []
+    for n in range(len(input_data.rose_frags)):
+        frag_mo_program = input_data.rose_frags[n].calc.name.lower()
+        fragments_mo_calculator.append(frag_mo_program)
+
+    version = input_data.version
+    exponent = input_data.exponent
+    restricted = input_data.restricted
+    test = input_data.test
+
+    avas_frag = input_data.avas_frag
+    nmo_avas = input_data.nmo_avas
+
+    # creating INPUT_GENIBO file
+    with open(genibo_filename, "w") as f:
+        f.write("**ROSE\n")
+        f.write(".VERSION\n")
+        f.write(version + "\n")
+        f.write(".CHARGE\n")
+        f.write(str(molecule_charge)+"\n")
+        f.write(".EXPONENT\n")
+        f.write(str(exponent) + "\n")
+        if not restricted:
+            f.write(".UNRESTRICTED\n")
+        f.write(".FILE_FORMAT\n")
+        f.write(molecule_mo_calculator + "\n")
+        if test:
+            f.write(".TEST  \n")
+        if avas_frag:
+            f.write(".AVAS  \n")
+        f.write(str(len(avas_frag)) + "\n")
+        f.writelines("{:3d}".format(item) for item in avas_frag)
+        f.write("\n*END OF INPUT\n")
+
+    # creating INPUT_AVAS file
+    if avas_frag:
+        with open(avas_filename, "w") as f:
+            f.write(str(molecule_natom) + " # natoms\n")
+            f.write(str(molecule_charge) + " # charge\n")
+            if restricted:
+                f.write("1 # restricted\n")
+            else:
+                f.write("0 # unrestricted\n")
+            f.write("1 # spatial orbs\n")
+            f.write(str(molecule_mo_calculator)
+                    + " # MO file for the full molecule\n")
+            f.write(" ".join(map(str, fragments_mo_calculator))
+                    + " # MO file for the fragments\n")
+            f.write(str(len(nmo_avas))
+                    + " # number of valence MOs in B2\n")
+            f.writelines("{:3d}".format(item) for item in nmo_avas)
+
+    # print(molecule_natom)
