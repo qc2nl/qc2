57c8027e75073414d96e8cd420cfcbe9304b0744
Cmurilochem <carlosmurilorocha@gmail.com>
2023-05-30 10:04:32 +0200

    Removed dummy rose-ase scripts

--- tests/clean_up.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-
-/bin/rm *xyz *dfcoef DFCOEF* *inp INPUT* MOLECULE.XYZ MRCONEE* *dfpcmo DFPCMO* *fchk *in fort.100 timer.dat INFO_MOL *.pyscf IAO_Fock SAO *.npy *.clean *\ 2* OUTPUT_AVAS *.chk ILMO*dat *.out OUTPUT_ROSE
