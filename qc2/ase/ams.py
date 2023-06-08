6bcec8f1701012bd8df96c47b50d8da658e9d5c6
Nico <nicolas.gm.renaud@gmail.com>
2023-06-08 10:58:26 +0200

    read MOs

--- qc2/ase/ams.py
+++ qc2/ase/ams.py
@@ -9,8 +9,32 @@ import os
 from ase import Atoms
 from ase.calculators.calculator import FileIOCalculator
 from ase.io import write
+import numpy as np 
 
 class AMS(AMSCalculator):
 
     def __init__(self, *args, **kwargs):
         super().__init__(args, kwargs)
+
+    def save(self, filename):
+        raise NotImplementedError
+    
+    def _get_molecular_orbitals(self) -> np.ndarray:
+        """Read the molecular orbitals from the rkf file
+        """
+
+        # number of mos and aos
+        naos = int(self.readrkf('Basis', 'naos', file='adf'))
+        nmos = int(self.readrkf('A', 'nmos_A', file='adf'))
+
+        # molecular orbital coefficients
+        mo_coeffs = np.array(self.readrkf('A', 'Eigen-Bas_A', file='adf')).reshape(nmos, naos)
+
+        # energy of the mos
+        energy = self.readrkf('A', 'eps_A', file='adf')
+
+        # ordering of the MOS
+        order = self.readrkf('A', 'npart', file='adf')
+        order = [o-1 for o in order]
+
+        return energy[order], mo_coeffs[:, order]
