72fb146661f9ea83b32ab7136ad3c9f8facff391
Cmurilochem <carlosmurilorocha@gmail.com>
2023-07-21 10:47:53 +0200

    Added final comments in schema.py

--- qc2/data/schema.py
+++ qc2/data/schema.py
@@ -38,3 +38,43 @@ def create_attributes(schema: Dict[str, Any], group: AttributeManager) -> None:
                                    dtype=h5py.special_dtype(vlen=str))
             else:
                 group.attrs.create(prop, None, dtype='f')
+
+
+# this is the original generate_empty_h5
+
+from h5json import Hdf5db
+from h5json.jsontoh5.jsontoh5 import Writeh5
+
+
+def original_generate_empty_h5(schema: str, h5name: str) -> None:
+    """Generate an empty HDF5 file from a JSON schema.
+
+    Args:
+        schema (str): Path to the JSON schema file.
+        h5name (str): Path to the output HDF5 file.
+    """
+    # open schema
+    text = open(schema).read()
+
+    # parse the json file into a python dictionary
+    h5json = json.loads(text)
+
+    if "root" not in h5json:
+        raise Exception("No 'root' key in the JSON schema.")
+    root_uuid = h5json["root"]
+
+    # create the file, will raise IOError if there's a problem
+    Hdf5db.createHDF5File(h5name)
+
+    with Hdf5db(
+        h5name, root_uuid=root_uuid, update_timestamps=False, app_logger=None
+    ) as db:
+        h5writer = Writeh5(db, h5json)
+        h5writer.writeFile()
+
+    # open with h5py and remove the _db_ group
+    # Note: this will delete any anonymous (un-linked) objects
+    f = h5py.File(h5name, "a")
+    if "__db__" in f:
+        del f["__db__"]
+    f.close()
