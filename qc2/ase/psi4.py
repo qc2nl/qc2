ca0089e9df5169f7be7633674dccce25b0a895e8
Cmurilochem <carlosmurilorocha@gmail.com>
2023-11-02 13:53:41 +0100

    Added load method

--- qc2/ase/psi4.py
+++ qc2/ase/psi4.py
@@ -5,7 +5,7 @@ https://databases.fysik.dtu.dk/ase/ase/calculators/psi4.html#module-ase.calculat
 """
 try:
     from ase.calculators.psi4 import Psi4 as Psi4_original
-    # from psi4.driver import fcidump
+    from psi4.driver import fcidump
 except ImportError as error:
     raise ImportError(
         "Failed to export original ROSE-Psi4 calculator!"
@@ -33,16 +33,51 @@ class Psi4(Psi4_original, BaseQc2ASECalculator):
 
         Give an example here on how to use Psi4....
         """
-        print("HEREEEE")
         Psi4_original.__init__(self, *args, **kwargs)
         BaseQc2ASECalculator.__init__(self)
 
+        self.scf_e = None
+        self.scf_wfn = None
+
     def save(self, datafile: Union[h5py.File, str]) -> None:
-        """Dumps qchem data to a datafile."""
+        """Dumps qchem data to a datafile using QCSchema or FCIDump formats.
+
+        Args:
+            datafile (Union[h5py.File, str]): file to save the data to.
+
+        Notes:
+            files are written following the QCSchema or FCIDump formats.
+
+        Returns:
+            None
+
+        Example:
+        >>> from ase.build import molecule
+        >>> from qc2.ase import Psi4
+        >>>
+        >>> molecule = molecule('H2')
+        >>> molecule.calc = Psi4(atoms=molecule, method='hf', basis='sto-3g')
+        >>> molecule.calc.schema_format = "qcschema"
+        >>> molecule.calc.get_potential_energy()
+        >>> molecule.calc.save('h2.hdf5')
+        >>>
+        >>> molecule = molecule('H2')
+        >>> molecule.calc = Psi4(atoms=molecule, method='hf', basis='sto-3g')
+        >>> molecule.calc.schema_format = "fcidump"
+        >>> molecule.calc.get_potential_energy()
+        >>> molecule.calc.save('h2.fcidump')
+        """
+        # save energy and wavefunction
+        method = self.parameters['method']
+        basis = self.parameters['basis']
+        self.scf_e, self.scf_wfn = self.psi4.energy(
+            f'{method}/{basis}', return_wfn=True
+        )
+
         # in case of fcidump format
-        # if self._schema_format == "fcidump":
-        #     fcidump(scf_wfn, datafile)
-        #     return
+        if self._schema_format == "fcidump":
+            fcidump(self.scf_wfn, datafile)
+            return
 
     def load(self, datafile: Union[h5py.File, str]) -> Union[
             QCSchema, FCIDump
@@ -62,13 +97,11 @@ class Psi4(Psi4_original, BaseQc2ASECalculator):
         >>> molecule = molecule('H2')
         >>> molecule.calc = Psi4(atoms=molecule, method='hf', basis='sto-3g')
         >>> molecule.calc.schema_format = "qcschema"
-        >>> molecule.calc.get_potential_energy()
-        >>> molecule.calc.save('h2.hdf5')
+        >>> qcschema = molecule.calc.load('h2.h5')
         >>>
         >>> molecule = molecule('H2')
         >>> molecule.calc = Psi4(atoms=molecule, method='hf', basis='sto-3g')
         >>> molecule.calc.schema_format = "fcidump"
-        >>> molecule.calc.get_potential_energy()
-        >>> molecule.calc.save('h2.fcidump')
+        >>> fcidump = molecule.calc.load('h2.fcidump')
         """
         return BaseQc2ASECalculator.load(self, datafile)
