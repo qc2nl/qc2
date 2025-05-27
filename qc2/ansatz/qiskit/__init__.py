de6898c858fff0f7239f8cb2a017b29f0e395b50
nicoreno <nicolas.gm.renaud@gmail.com>
2025-05-27 18:32:32 +0200

    refac

--- qc2/ansatz/qiskit/__init__.py
+++ qc2/ansatz/qiskit/__init__.py
@@ -1,3 +1,4 @@
-from .lucj_ansatz import LUCJ
-from .qnp_gfa import GateFabric
-from .ansatz import create_ansatz 
+from .lucj import LUCJ
+from .gate_fabric import GateFabric
+
+__all__ = ["LUCJ", "GateFabric"]
