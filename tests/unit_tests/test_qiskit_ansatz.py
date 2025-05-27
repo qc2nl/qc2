ff60ac7bce29fbed8582c958f47befb3d60d168c
nicoreno <nicolas.gm.renaud@gmail.com>
2025-05-27 18:58:34 +0200

    remove import

--- tests/unit_tests/test_qiskit_ansatz.py
+++ tests/unit_tests/test_qiskit_ansatz.py
@@ -1,9 +1,8 @@
 """Tests for the qiskit ansatz"""
 from qiskit_nature.second_q.circuit.library import HartreeFock
 from qiskit_nature.second_q.mappers import JordanWignerMapper
-from qc2.ansatz.qiskit import LUCJ, GateFabric, create_ansatz
+from qc2.ansatz.qiskit import LUCJ, GateFabric
 import pyscf
-import ffsim
 
 def test_gate_fabric():
     num_spatial_orbitals = 4
