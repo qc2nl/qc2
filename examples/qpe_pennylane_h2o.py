f6ac97506efe86cebd9a5db1d24ef17764d8e99d
nicoreno <nicolas.gm.renaud@gmail.com>
2025-06-11 11:59:40 +0200

    add examples

---
+++ examples/qpe_pennylane_h2o.py
@@ -0,0 +1,45 @@
+"""Example of a VQE calc using Qiskit-Nature and PYSCF-ASE calculator.
+
+Standard restricted calculation => H2O example.
+
+Notes:
+    Requires the installation of qc2, ase, qiskit and h5py.
+"""
+
+from ase.build import molecule
+
+from qc2.ase import PySCF
+from qc2.data import qc2Data
+from qc2.algorithms.pennylane import QPE
+from qc2.algorithms.utils import ActiveSpace
+
+
+# set Atoms object
+mol = molecule("H2O")
+
+# file to save data
+hdf5_file = "h2o_ase_pyscf_pennylane.hdf5"
+
+# init the hdf5 file
+qc2data = qc2Data(hdf5_file, mol)
+
+# specify the qchem calculator
+qc2data.molecule.calc = PySCF()  # default => RHF/STO-3G
+
+# run calculation and save qchem data in the hdf5 file
+qc2data.run()
+
+# set up VQE calc
+qc2data.algorithm = QPE(
+    active_space=ActiveSpace(
+        num_active_electrons=(1, 1), 
+        num_active_spatial_orbitals=2
+    ),
+    num_evaluation_qubits=3
+)
+
+# run the calc
+result = qc2data.algorithm.run(
+    device_kwargs={"shots": None},
+    qnode_kwargs={"diff_method": "backprop"}
+)
