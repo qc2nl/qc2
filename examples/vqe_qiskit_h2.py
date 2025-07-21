82459d392e009044912e35878eed82554076628f
nicoreno <nicolas.gm.renaud@gmail.com>
2025-07-21 15:53:49 +0200

    fix ex algo calls

--- examples/vqe_qiskit_h2.py
+++ examples/vqe_qiskit_h2.py
@@ -16,7 +16,7 @@ from qc2.ase import DIRAC
 from qc2.qc2_driver import QC2 as qc2Data
 from qc2.algorithms.qiskit import VQE
 from qc2.second_q.active_space import ActiveSpace
-
+from qc2.qubit_mappers.qiskit.jordan_wigner import JordanWigner
 
 def clean_up_DIRAC_files():
     """Remove DIRAC calculation outputs."""
@@ -44,7 +44,7 @@ qc2data.algorithm = VQE(
     active_space=ActiveSpace(
         num_active_electrons=(1, 1), num_active_spatial_orbitals=2
     ),
-    mapper="bk",
+    mapper=JordanWigner(),
     optimizer=SLSQP(),
     estimator=Estimator(),
 )
