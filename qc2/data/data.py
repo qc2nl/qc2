c4241caf38d3518acbf81251f60617b2aebd0030
nicoreno <nicolas.gm.renaud@gmail.com>
2025-07-22 11:00:35 +0200

    print ao integrals

--- qc2/data/data.py
+++ qc2/data/data.py
@@ -612,6 +612,13 @@ class qc2Data:
         if transform is True:
             if initial_es_problem is not None:
                 # Transform `ElectronicStructureProblem` to a new basis
+
+                print('1body AO Integrals alpha[+-]: ', initial_es_problem.hamiltonian.electronic_integrals.one_body.alpha['+-'])
+                print('1body AO Integrals beta[+-]: ', initial_es_problem.hamiltonian.electronic_integrals.one_body.beta.get('+-',0))
+                print('1body AO Integrals alpha[++--]: ', initial_es_problem.hamiltonian.electronic_integrals.one_body.alpha.get('++--',0))
+                print('1body AO Integrals beta[++--]: ', initial_es_problem.hamiltonian.electronic_integrals.one_body.beta.get('++--',0))
+                # print('2body aactive Integrals: ',  initial_es_problem.hamiltonian.electronic_integrals.two_body.alpha['++--'])
+                exit()
                 transformed_es_problem, _ = self.get_transformed_hamiltonian(
                     initial_es_problem=initial_es_problem,
                     matrix_transform_a=matrix_transform_a,
@@ -634,9 +641,9 @@ class qc2Data:
          )
         
         print('Core Energy: ', core_energy)
-        print('1body active Integrals: ', es_problem.hamiltonian.electronic_integrals.one_body.alpha['+-'])
-        print('2body ctive Integrals: ',  es_problem.hamiltonian.electronic_integrals.two_body.alpha['++--'])
-        exit()
+        # print('1body active Integrals: ', es_problem.hamiltonian.electronic_integrals.one_body.alpha['+-'])
+        # print('2body aactive Integrals: ',  es_problem.hamiltonian.electronic_integrals.two_body.alpha['++--'])
+        # exit()
 
         # now convert the reduced Hamiltonian (`Hamiltonian` instance)
         # into a `FermionicOp` instance
