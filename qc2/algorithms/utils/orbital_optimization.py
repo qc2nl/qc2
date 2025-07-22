ca696632d3b71a6aa23d0e51e7fd0142e7b500d3
nicoreno <nicolas.gm.renaud@gmail.com>
2025-07-22 08:46:49 +0200

    print transformed integrals

--- qc2/algorithms/utils/orbital_optimization.py
+++ qc2/algorithms/utils/orbital_optimization.py
@@ -634,8 +634,9 @@ class OrbitalOptimization():
         )
         ## BUG THIS NEEDS TO BE IN THE AO BASIS
         hamiltonian_atomic_basis = ElectronicHamiltonian(schema=self.schema_dataclass, basis='atomic')
-        print('Atomic Integrals: ', hamiltonian_atomic_basis.electronic_integrals.one_body.alpha.get('+-'))
         hamiltonian_molecular_basis = basis_transformer.transform_hamiltonian(hamiltonian_atomic_basis)
+        print('1body Atomic Integrals: ', hamiltonian_molecular_basis.electronic_integrals.one_body.alpha.get('+-'))
+        print('2body Atomic Integrals: ', hamiltonian_molecular_basis.electronic_integrals.two_body.alpha.get('++--'))
 
         core_energy, active_space_hamiltonian = self.qc2data.get_active_space_hamiltonian(
             self.n_active_electrons,
