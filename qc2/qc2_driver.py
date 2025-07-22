1afcc7ae4eda150c6e3870ec3b90311af1f63b93
nicoreno <nicolas.gm.renaud@gmail.com>
2025-07-22 09:11:03 +0200

    print inactive core energy

--- qc2/qc2_driver.py
+++ qc2/qc2_driver.py
@@ -310,6 +310,8 @@ class QC2:
         inactive_space_energy = active_space_hamiltonian.constants[
             "ActiveSpaceTransformer"
         ]
+        print('nuclear_repulsion_energy', nuclear_repulsion_energy)
+        print('inactive_space_energy', inactive_space_energy)
         core_energy = nuclear_repulsion_energy + inactive_space_energy
 
         return core_energy, active_space_hamiltonian
