1eb4de1d2370af88ae685db59ee5acc879472a54
nicoreno <nicolas.gm.renaud@gmail.com>
2025-08-22 15:46:26 +0200

    fix test

--- tests/unit_tests/test_rose_ase.py
+++ tests/unit_tests/test_rose_ase.py
@@ -4,13 +4,14 @@ import shutil
 import pytest
 
 from qc2.data.fcidump import FCIDump
-from qc2.ase import ROSE, ROSETargetMolecule, ROSEFragment
 
 # try:
 #     from qc2.ase import ROSE, ROSETargetMolecule, ROSEFragment
 # except ImportError:
 #     pytest.skip("Skipping ASE-ROSE tests...",
 #                 allow_module_level=True)
+
+from qc2.ase import ROSE, ROSETargetMolecule, ROSEFragment
 
 # also check if the `genibo.x` and `avas.x` executables are available
 # if not shutil.which("genibo.x") or not shutil.which("avas.x"):
@@ -38,38 +39,51 @@ def clean_up_files():
 @pytest.fixture
 def rose_calculator():
     """Water atom_frag example calculation."""
-    h2o = ROSETargetMolecule(
-        name='water',
-        atoms=[('O', (0.,  0.00000,  0.59372)),
-               ('H', (0.,  0.76544, -0.00836)),
-               ('H', (0., -0.76544, -0.00836))],
+    NH3OH2 = ROSETargetMolecule(
+        name='water-ammonia',
+        atoms=[('N', (-1.39559, -0.02156,  0.00004)),
+            ('H', (-1.62981,  0.96110, -0.10622)),
+            ('H', (-1.86277, -0.51254, -0.75597)),
+            ('H', (-1.83355, -0.33077,  0.86231)),
+            ('O', (1.56850,  0.10589,  0.00001)),
+            ('H', (0.60674, -0.03396, -0.00063)),
+            ('H', (1.94052, -0.78000,  0.000))],
         basis='sto-3g'
     )
 
-    oxygen = ROSEFragment(
-        name='oxygen',
-        atoms=[('O', (0, 0, 0))],
-        multiplicity=1, basis='sto-3g'
+    NH3 = ROSEFragment(
+        name='ammonia',
+        atoms=[('N', (-1.39559, -0.02156,  0.00004)),
+            ('H', (-1.62981,  0.96110, -0.10622)),
+            ('H', (-1.86277, -0.51254, -0.75597)),
+            ('H', (-1.83355, -0.33077,  0.86231))],
+        basis='sto-3g'
     )
 
-    hydrogen = ROSEFragment(
-        name='hydrogen',
-        atoms=[('H', (0, 0, 0))],
-        multiplicity=2, basis='sto-3g'
+    H2O = ROSEFragment(
+        name='water',
+        atoms=[('O', (1.56850,  0.10589,  0.00001)),
+            ('H', (0.60674, -0.03396, -0.00063)),
+            ('H', (1.94052, -0.78000,  0.000))],
+        basis='sto-3g'
     )
 
-    h2o_calculator = ROSE(rose_calc_type='atom_frag',
-                          exponent=4,
-                          rose_target=h2o,
-                          rose_frags=[oxygen, hydrogen],
-                          test=True,
-                          avas_frag=[0],
-                          nmo_avas=[3, 4, 5],
-                          save_data=True,
-                          restricted=True,
-                          openshell=True,
-                          rose_mo_calculator='pyscf')
-    return h2o_calculator
+    H2O_calculator = ROSE(
+        rose_calc_type='mol_frag',
+        exponent=2,
+        rose_target=NH3OH2,
+        rose_frags=[NH3, H2O],
+        test=True,
+        additional_virtuals_cutoff=2.0,
+        frag_threshold=10.0,
+        frag_valence=[[1, 7], [2, 6]],
+        frag_core=[[1, 1], [2, 1]],
+        avas_frag=[1],
+        nmo_avas=[[2, 3, 4]],
+    )
+    H2O_calculator.get_potential_energy()
+
+    return H2O_calculator
 
 
 def test_ROSE_load_function(rose_calculator):
